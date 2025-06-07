8847885d747db002556f417a6148a7b5fa7e868e
Little-Blueberries <nicknameabc4@gmail.com>
2025-06-07 18:48:52 +0700

    modified:   Makefile 	deleted:    build/bootloader.bin 	deleted:    build/kernel.bin 	deleted:    build/main_floppy.img 	deleted:    build/tools/fat 	new file:   src/bootloader/stage1/Makefile 	renamed:    src/bootloader/boot.asm -> src/bootloader/stage1/boot.asm 	new file:   src/bootloader/stage2/Makefile 	new file:   src/bootloader/stage2/linker.lnk 	new file:   src/bootloader/stage2/main.asm 	new file:   src/kernel/Makefile 	deleted:    tools/fat/fat.c

---
+++ src/bootloader/stage2/main.asm
@@ -0,0 +1,50 @@
+org 0x0
+bits 16
+
+
+%define ENDL 0x0D, 0x0A
+
+
+start:
+    ; print hello world message
+    mov si, msg_hello
+    call puts
+
+    ; Print another message
+    mov si, msg_demo
+    call puts
+
+.halt:
+    cli
+    hlt
+
+;
+; Prints a string to the screen
+; Params:
+;   - ds:si points to string
+;
+puts:
+    ; save registers we will modify
+    push si
+    push ax
+    push bx
+
+.loop:
+    lodsb               ; loads next character in al
+    or al, al           ; verify if next character is null?
+    jz .done
+
+    mov ah, 0x0E        ; call bios interrupt
+    mov bh, 0           ; set page number to 0
+    int 0x10
+
+    jmp .loop
+
+.done:
+    pop bx
+    pop ax
+    pop si    
+    ret
+
+msg_hello:  db 'Hello world from KERNEL!', ENDL, 0
+msg_demo:   db 'This is just a demo system, see us again later with super graphics update!', ENDL, 0
